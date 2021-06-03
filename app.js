eca37d121510d08a6af57e76c336bf6a4d2f75b1
mavisalli <mavi.baris96@gmail.com>
2021-06-03 20:15:27 +0300

    public and views files added

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
+
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+  res.status(200).render("index", {
+      page_name: "index"
+  });
 });
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about", {
+        page_name: "about"
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
