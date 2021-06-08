97c92058464af26034bd0f25aa9ba87293387e71
mavisalli <mavi.baris96@gmail.com>
2021-06-08 16:20:35 +0300

    Course - student Relationship added and search field activated

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require("../models/Course");
 const Category = require("../models/Category");
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
   try {
@@ -26,6 +27,7 @@ exports.getAllCourses = async (req, res) => {
   try {
 
     const categorySlug = req.query.categories; 
+    const query = req.query.search; 
     const category = await Category.findOne({slug: categorySlug});  // parametreden gelen category ismini buluyoruz.
 
     let filter = {}; //ileride search bölümünden de category quwry'si olusturcagımız icin bos bir filter olusturduk.
@@ -33,7 +35,22 @@ exports.getAllCourses = async (req, res) => {
     if(categorySlug) {
       filter = {category: category._id} //mesela category._id si web design'ın objectId sine esit olanları filtrele.
     }
-    const courses = await Course.find(filter).sort("-createdAt"); // En son yüklenenin basta gözükmesini saglar
+
+    if(query) {
+      filter = {name: query}
+    }
+
+    if(!query && !categorySlug) {
+      filter.name = "",
+      filter.category = null
+    }
+
+    const courses = await Course.find({
+      $or:[ // ikisinden birine göre sec
+        {name: { $regex: '.*' + filter.name + '.*', $options: 'i'}}, // $options: "i" kücük büyük harfe duyarsız yapar
+        {category: filter.category}
+      ]
+    }).sort("-createdAt").populate("user"); // sort sayesinde En son yüklenenin basta gözükmesini saglar
 
     const categories = await Category.find();
 
@@ -52,15 +69,52 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
+    const user = await User.findById(req.session.userID);
     const course = await Course.findOne({ slug: req.params.slug }).populate("user"); //course'ın icindeki user'dan faydalanıp tekil course sayfasında user'a ulasıyoruz. populate sayesinde
+    const categories = await Category.find();
 
     res.status(200).render("course", {
       course,
       page_name: "courses",
+      user,
+      categories
     });
   } catch (error) {
     res.status(400).json({
       status: "fail",
+      error,
+    });
+  }
+};
+
+exports.enrollCourse = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+
+exports.releaseCourse = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
       error,
     });
   }
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -12,6 +12,8 @@ router.route("/").post(roleMiddleware(["teacher","admin"]),courseController.crea
 
 router.route("/").get(courseController.getAllCourses);
 router.route("/:slug").get(courseController.getCourse);
+router.route("/enroll").post(courseController.enrollCourse);
+router.route("/release").post(courseController.releaseCourse);
 
 
 module.exports = router;
