8d266f8bcb1c2017985ef296ee8c7971f2dc4a37
mavisalli <mavi.baris96@gmail.com>
2021-06-10 02:17:49 +0300

    Adding and deleting categories in the admin panel has been created

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,6 +5,8 @@ const categoryController = require("../controllers/categorycontroller");
 const router = express.Router();
 
 router.route("/").post(categoryController.createCategory); 
+router.route("/:id").delete(categoryController.deleteCategory);
+
 
 
 
